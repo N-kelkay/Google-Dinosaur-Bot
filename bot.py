5450eb8caaf163a4589ac28cb81eabb1d7f95509
Natenael Kelkay <natikelkay@gmail.com>
2018-06-17 16:37:34 -0400

    made the t-rex jump

--- bot.py
+++ bot.py
@@ -1,10 +1,25 @@
 import pyautogui
 import time
 
-class coordinate():
+
+class coordinate ():
     replyBtn = (357, 365)
+    dinosaur = (146, 369)
+
 
 def restartGame():
     pyautogui.doubleClick(coordinate.replyBtn)
 
+
+def pressSpace():
+    pyautogui.keyDown('space')
+    time.sleep(0.05)
+    print("Jump")
+    pyautogui.keyUp('space')
+
+
 restartGame()
+
+time.sleep(1)
+
+pressSpace()
